3f62fb81171ee8e08ff28e77091196cbf2279995
유현진 <uhyeonjin0619@gmail.com>
2024-05-21 09:52:55 +0900

    Employee 클래스 상속 및 다형성
    Employee 클래스를 상속받는 HourlyEmployee클래스와 SalariedEmployee 클래스를 만들고 Employee 클래스의 참조변수 배열을 만들어 Scanner로 어떤 자식 클래스 객체로 만들지 사용자로부터 입력받는다.
    computeSalary 메서드를 실행하는것은 동일하지만 내부의 코드는 두 개의 자식 클래스가 각각 다르므로 다형성을 확인할 수 있다!

---
+++ day0520/src/com/kosta/exam07/EmployeeTest2.java
@@ -0,0 +1,67 @@
+package com.kosta.exam07;
+import java.util.Scanner;
+
+public class EmployeeTest2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Employee []employee = new Employee[1000];
+		int level, workingHour, hourlyRate, type, i=0;
+		String name="", no="";
+		
+		while(true) {
+			
+			while(true) {
+				System.out.println("사원의 급여 종류를 입력해주세요");
+				System.out.println("[0] 종료");
+				System.out.println("[1] 월급제");
+				System.out.println("[2] 시간제");
+				type = sc.nextInt();
+				
+				if (type>=0 && type<=2) {
+					break;
+				}
+			}
+			if (type==0) {
+				break;
+			}
+			
+			System.out.print("사원의 이름을 입력해주세요 : ");
+			name = sc.next();
+			System.out.print("사원번호를 입력해주세요 : ");
+			no = sc.next();
+			
+			switch(type) {
+			case 1 :
+				System.out.println("사원의 호봉을 입력해주세요");
+				System.out.println("[1] [2] [3]");
+				while(true) {
+					level = sc.nextInt();
+					if (level>=1&&level<=3) {
+						break;
+					}
+					System.out.println("호봉을 범위 내에서 입력해주세요");
+				}
+				employee[i] = new SalariedEmployee(name, no, level);
+				employee[i].computeSalary();
+				break;
+			case 2 :
+				System.out.println("근무시간을 입력해주세요");
+				workingHour = sc.nextInt();
+				System.out.println("시급을 입력해주세요");
+				hourlyRate= sc.nextInt();
+				employee[i] = new HourlyEmployee(name, no, workingHour, hourlyRate);
+				employee[i].computeSalary();
+				break;
+				
+			}//end switch
+			i++;
+		}//end while
+		System.out.println("작업종료");
+		
+		for (int j = 0; j < i; j++) {
+			System.out.println(employee[j]);
+			
+		}//end for
+	}//end main
+}
