f26e327cdd836155d5e7a536eff873bd4eaf7421
유현진 <uhyeonjin0619@gmail.com>
2024-05-22 15:49:48 +0900

    자바 gui
    frame 클래스 객체 생성해서 띄워보기

---
+++ day0522/src/com/kosta/exam06/FrameTest.java
@@ -0,0 +1,14 @@
+package com.kosta.exam06;
+
+import java.awt.Frame;
+
+public class FrameTest {
+
+	public static void main(String[] args) {
+		Frame f = new Frame("new frame");
+		f.setSize(400, 300);
+		f.setVisible(true);
+		
+	}
+
+}
