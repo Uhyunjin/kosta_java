dd7ad802a9de708285c0fca0aab95fc2a9dbdf17
유현진 <uhyeonjin0619@gmail.com>
2024-05-23 09:31:51 +0900

    final 클래스는 상속받을수없다

--- day0523/src/com/kosta/exam01/FinalTest.java
+++ day0523/src/com/kosta/exam01/FinalTest.java
@@ -2,13 +2,20 @@ package com.kosta.exam01;
 
 class A{
 	public void pro() {
-		System.out.println("프로입니다");
+		System.out.println("A클래스의 프로입니다");
+	}
+	public final void test() {
+		System.out.println("A클래스의 test입니다");
 	}
 }
 class B extends A{
 	public void pro() {
 		System.out.println("B클래스의 프로입니다");
 	}
+//	public void test() { //에러
+//		System.out.println("A클래스의 test입니다");
+//	}
+	//부모클래스의 final 메서드는 오버라이딩 불가능
 }
 
 public class FinalTest {
