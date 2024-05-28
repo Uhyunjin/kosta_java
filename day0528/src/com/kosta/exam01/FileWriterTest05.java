ef670ff84e064ef78af72c8facfd1dd4b015ce3c
유현진 <uhyeonjin0619@gmail.com>
2024-05-28 12:50:16 +0900

    throws로 예외처리

---
+++ day0528/src/com/kosta/exam01/FileWriterTest05.java
@@ -0,0 +1,24 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+class FileWriterTest05
+{
+	public static void pro(String fname)throws IOException{
+
+		FileWriter fw = new FileWriter(fname);
+
+		fw.write("hello java");
+		fw.close();
+		System.out.println("파일을 생성하였습니다");
+
+	}
+
+	public static void main(String[] args) 
+	{	
+		try{
+		pro("c:/daka/hello.txt");
+		}catch(IOException e){
+			System.out.println(e.getMessage());
+		}
+	}
+}
