462fbb4529cb1c7d1d5b649f06acec450b4a7734
유현진 <uhyeonjin0619@gmail.com>
2024-05-29 11:29:07 +0900

    thread 상속 카운트다운

---
+++ day0529/src/com/kosta/exam06/CountDownThreadTest.java
@@ -0,0 +1,11 @@
+package com.kosta.exam06;
+
+public class CountDownThreadTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new CountDownThread().start();
+		
+	}
+
+}
