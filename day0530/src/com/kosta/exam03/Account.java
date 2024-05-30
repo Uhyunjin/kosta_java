478ecf51f1616631fd7fa52ffa4cfeff80e3547f
유현진 <uhyeonjin0619@gmail.com>
2024-05-30 10:47:21 +0900

    join메서드를 사용하지 않을때
    스레드가 끝나기 전에 print메서드가 실행된다

---
+++ day0530/src/com/kosta/exam03/Account.java
@@ -0,0 +1,14 @@
+package com.kosta.exam03;
+
+public class Account {	//모금액을 위한 클래스
+	
+	private int balance;	//잔액을 위한 변수
+	
+	public void call(int amount) {	//입금을 위한 메서드
+		balance+=amount;
+	}
+	
+	public int getBalance() {
+		return balance;
+	}
+}
