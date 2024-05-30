33903bcc30837d9e147980e9a3370db4efef5978
유현진 <uhyeonjin0619@gmail.com>
2024-05-30 15:33:12 +0900

    flow layout

--- day0530/src/com/kosta/exam11/MyFrame.java
+++ day0530/src/com/kosta/exam11/MyFrame.java
@@ -1,28 +1,28 @@
-package com.kosta.exam10;
+package com.kosta.exam11;
 
+import java.awt.Component;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class MyFrame extends JFrame {
-	
-	JButton button01;
-	JButton button02;
+
+	JButton []button = new JButton[15];
 	
 	public MyFrame() {
-		setLayout(new FlowLayout());
-		button01=new JButton("남자");
-		button02=new JButton("여자");
-		add(button01);
-		add(button02);
+		setLayout(new GridLayout(5,3));
+		for (int i = 1; i < 16; i++) {
+			add(new JButton("버튼" +i));
+		}
+
+		
 		setSize(500,500);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-	
-		
 	}
-	
 	
 	public static void main(String[] args) {
 		new MyFrame();
