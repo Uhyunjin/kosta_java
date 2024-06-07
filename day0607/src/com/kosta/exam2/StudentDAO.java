55f0b5c1c570600bd84b83c7339dc7da8eb88ed0
유현진 <uhyeonjin0619@gmail.com>
2024-06-07 12:02:15 +0900

    DAO select method

--- day0607/src/com/kosta/exam2/StudentDAO.java
+++ day0607/src/com/kosta/exam2/StudentDAO.java
@@ -2,8 +2,10 @@ package com.kosta.exam2;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class StudentDAO {
 
@@ -46,5 +48,36 @@ public class StudentDAO {
 		}
 
 		return re;
+	}
+	
+	//모든 학생에 대한 정보를 조회하여 ArrayList로 반환하는 메서드 만들기
+	public ArrayList<StudentVO> listStudent() {
+		ArrayList<StudentVO> list = new ArrayList<StudentVO>();
+		sql = "select * from student";
+		ResultSet rs = null;
+		try {
+			Class.forName(driver);
+			conn = DriverManager.getConnection(url);
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(sql);
+			
+			while (rs.next()) {
+				rs.next();
+				list.add(new StudentVO(rs.getString(1),
+										rs.getInt(2),
+										rs.getInt(3),
+										rs.getInt(4)));
+			}
+			
+		} catch (Exception e) {
+			e.getMessage();
+		} finally {
+			try {
+				if (rs!=null) {rs.close();}
+				if (stmt!=null) {stmt.close();}
+				if (conn!=null) {conn.close();}
+			} catch (Exception e2) {e2.getMessage();}
+		}
+		return list;
 	}
 }
